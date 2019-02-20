1ac7d81bf41fd7b466761ec7c665515e802fdc66
Ivan <plastun.vanya@yandex.ru>
2019-02-20 12:07:41 +0300

    landing icndy was completed

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() {
+    
+    var nav = $('.nav');  
+    $(document).on('click', '.hide-nav', function() {
+        $('.hide-nav').toggleClass('active');
+        if(nav.is(':not(:visible)'))
+            nav.slideDown();
+        else 
+            nav.slideUp();
+    });
+});
